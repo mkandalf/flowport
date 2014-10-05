5055c442deae1f1b5c7c41f8c013f445c501201f
Nick Meyer <nicholasmeyer@gmail.com>
2014-10-04 20:26:13 -0400

    Smoothing

--- src/run.js
+++ src/run.js
@@ -6,9 +6,19 @@ var position = {x: 0, y: 0};
 var lastT = (new Date()).getTime();
 var multiplicativeFactor = 0.001;
 
+var lastVelocity;
+
+var alpha = 0.5;
+
 
 flow.onCalculated(
 	function (direction) {
+		if (!lastVelocity) {
+			lastVelocity = {u: direction.u, v: direction.v};
+		}
+		var smoothedU = alpha * direction.u + (1 - alpha) * lastVelocity.u;
+		var smoothedV = alpha * direction.v + (1 - alpha) * lastVelocity.v;
+		lastVelocity = {u : smoothedU, v: smoothedV};
 	    console.log(JSON.stringify(direction, null, 4));
 	    var currentT = (new Date()).getTime();
 	    var deltaT = currentT - lastT;
