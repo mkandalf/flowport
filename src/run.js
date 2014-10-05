c3e785d907b9558ccaffbf4bace1fccc2d0f6581
Zain Shah <zan2434@gmail.com>
2014-10-04 20:55:07 -0400

    added if condition for direction being NaN, and changed mult factor

--- src/run.js
+++ src/run.js
@@ -4,7 +4,7 @@ var flow = new oflow.WebCamFlow(window.document.createElement('video'), 32);
 var position = {x: 0, y: 0};
 
 var lastT = (new Date()).getTime();
-var multiplicativeFactor = 0.1;
+var multiplicativeFactor = 0.5;
 
 var smoothed;
 
@@ -13,21 +13,24 @@ var alpha = 0.5;
 
 flow.onCalculated(
 	function (direction) {
-		if (!smoothed) {
-			smoothed = {u: direction.u, v: direction.v};
+		if(isNaN(direction.u) || isNaN(direction.v))
+			document.getElementById("x").innerHTML = "direction was not a number";
+		else{
+			if (!smoothed) {
+				smoothed = {u: direction.u, v: direction.v};
+			}
+			smoothed = {
+				u: alpha * direction.u + (1 - alpha) * smoothed.u,
+				v: alpha * direction.v + (1 - alpha) * smoothed.v
+			};
+		    var currentT = (new Date()).getTime();
+		    var deltaT = currentT - lastT;
+		    position.x += smoothed.u*deltaT/1000*multiplicativeFactor;
+		    position.y += smoothed.v*deltaT/1000*multiplicativeFactor;
+		    document.getElementById("x").innerHTML = position.x;
+		    document.getElementById("y").innerHTML = position.y;
+		    document.getElementById("image").style.backgroundPosition = position.x+" "+position.y;
 		}
-		smoothed = {
-			u: alpha * direction.u + (1 - alpha) * smoothed.u,
-			v: alpha * direction.v + (1 - alpha) * smoothed.v
-		};
-	    console.log(JSON.stringify(direction, null, 4));
-	    var currentT = (new Date()).getTime();
-	    var deltaT = currentT - lastT;
-	    position.x += smoothed.u*deltaT/1000*multiplicativeFactor;
-	    position.y += smoothed.v*deltaT/1000*multiplicativeFactor;
-	    document.getElementById("x").innerHTML = position.x;
-	    document.getElementById("y").innerHTML = position.y;
-	    document.getElementById("image").style.backgroundPosition = position.x+" "+position.y;
 	}
 );
 
